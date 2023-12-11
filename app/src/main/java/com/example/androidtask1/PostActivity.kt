49d6efa590b46d4f9959e8f0221c1357a206dad1
manar <96601859+manarkhaledx@users.noreply.github.com>
2023-12-11 23:55:51 +0200

    both fb login & fb post layouts created

---
+++ app/src/main/java/com/example/androidtask1/PostActivity.kt
@@ -0,0 +1,12 @@
+package com.example.androidtask1
+
+import android.os.Bundle
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+
+public class PostActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState:Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_post)
+    }
+}
